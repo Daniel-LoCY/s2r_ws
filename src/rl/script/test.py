df804cc255d6964b9bac2299f9d5b1a512bf1eff
Daniel <game48875@gmail.com>
2024-06-03 14:42:42 +0800

    fix test.py

--- src/rl/script/test.py
+++ src/rl/script/test.py
@@ -1,16 +1,5 @@
-# import roslaunch
-
-# uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-# roslaunch.configure_logging(uuid)
-
-# launch1 = roslaunch.parent.ROSLaunchParent(uuid, ["src/rl/launch/static_tf.launch"])
-# # launch2 = roslaunch.parent.ROSLaunchParent(uuid, ["/path/to/launch2.launch"])
-
-# launch1.start()
-# # launch2.start()
-
-
 #!/usr/bin/env python3
+
 from utils import *
 import rospy
 
